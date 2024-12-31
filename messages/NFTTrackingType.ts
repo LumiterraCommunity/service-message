35d8177665cf215285704665633fd73a1fd7f76c
adrian <jason.lumiterra@gmail.com>
2024-12-31 18:57:45 +0800

    feat(messages): 添加 NFT Tracking Type 枚举 用于标识产出-消耗的场景来源

---
+++ messages/NFTTrackingType.ts
@@ -0,0 +1,21 @@
+
+
+// 用于标识产出-消耗的场景来源
+export enum NFTTrackingType {
+    // 默认类型
+    Default = 'Default',
+    // 装备强化
+    EnhanceEquipment = 'EnhanceEquipment',
+    // 爬塔
+    TowerDungeon = 'TowerDungeon',
+    // 合成
+    Merge = 'Merge',
+    // 怪物掉落 (大世界)
+    MonsterDrop = 'MonsterDropWold',
+    // 怪物掉落 (家园)
+    MonsterDropHome = 'MonsterDropHome',
+    // 怪物掉落 (副本)
+    MonsterDropDungeon = 'MonsterDropDungeon',
+    // 任务
+    Task = 'Task',
+}
